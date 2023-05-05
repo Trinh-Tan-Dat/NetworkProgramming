a4fe3b232f6af55567dd7d74fef09e5a41a9691b
Đạt Trịnh Tấn <21520714@ms.uit.edu.vn>
2023-05-05 16:49:34 +0700

    Đề 2: UDP translate

---
+++ De2/client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace De2
+{
+    public partial class client : Form
+    {
+        private UdpClient udpClient;
+        IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+
+        public client()
+        {
+            InitializeComponent();
+            
+            
+        }
+        public void ReceiveMessage()
+        {
+            while (true)
+            {
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] receiveMess = udpClient.Receive(ref remoteEP);
+                String message = Encoding.UTF8.GetString(receiveMess);
+                //richTextBox1.Invoke(new Action(() => { richTextBox1.Text = message; }));
+                richTextBox1.Text = String.Concat(richTextBox1.Text, message + "\n");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            udpClient = new UdpClient();
+            Byte[] sendBytes = Encoding.UTF8.GetBytes(textBox1.Text);
+            udpClient.Send(sendBytes, sendBytes.Length, serverEndpoint);
+
+            Thread.Sleep(300);
+            
+            Thread thread = new Thread(new ThreadStart(ReceiveMessage));
+            thread.Start();
+
+        }
+    }
+}
---
+++ De2/server.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Script.Serialization;
+namespace De2
+{
+    public partial class server : Form
+    {
+        public server()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void serverThread()
+        {
+
+            UdpClient udpClient = new UdpClient(8080);
+            try
+            {
+                while (true)
+                {
+                    IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    
+                    Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+                    string mess = RemoteIpEndPoint.Address.ToString() + ":" + returnData.ToString();
+                    InfoMessage(mess);
+                    //
+
+
+                    string url = String.Format
+                    ("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+                     "en", "vi", Uri.EscapeUriString(mess));
+                    HttpClient httpClient = new HttpClient();
+                    string result = httpClient.GetStringAsync(url).Result;
+                    var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
+                    var translationItems = jsonData[0];
+                    string translation = "";
+                    foreach (object item in translationItems)
+                    {
+                        IEnumerable translationLineObject = item as IEnumerable;
+                        IEnumerator translationLineString = translationLineObject.GetEnumerator();
+                        translationLineString.MoveNext();
+                        translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
+                    }
+                    if (translation.Length > 1) { translation = translation.Substring(1); };
+
+
+                    //
+                    byte[] responseData = Encoding.UTF8.GetBytes("Server: " + translation);
+                    udpClient.Send(responseData, responseData.Length, RemoteIpEndPoint);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.ToString());
+            }
+        }
+        public void InfoMessage(String mess)
+        {
+            richTextBox1.Text = String.Concat(richTextBox1.Text, mess + "\n");
+        }
+
+        private void server_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread thdUDPServer = new Thread(new ThreadStart(serverThread));
+            thdUDPServer.Start();
+        }
+    }
+}
